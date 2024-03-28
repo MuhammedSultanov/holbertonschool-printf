328a44de419f4d8ba65766a4dff169476e0fed48
Muhammed Sultanov <9073@holbertonstudents.com>
2024-03-28 12:59:28 +0000

    This file is for the print char

---
+++ _printf_c.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _print_c - A function for printing characters
+ * @a: leng
+ * @b: char
+ * */
+int _print_c(unsigned int *a, char b)
+{
+    if (!b && b != '\0')
+    {
+        write(1, "(null)", 6);
+        *a += 6;
+    }
+    else
+    {
+        write(1, &b, 1);
+        (*a)++;
+    }
+    return (*a);
+}
