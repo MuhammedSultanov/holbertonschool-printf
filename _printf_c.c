a81f925e2f7421f5eb05876246c80b3205770454
sohrab <syusibov92@gmail.com>
2024-03-30 03:53:12 -0700

    we fixed missing of return value description error and some basic betty warnings

--- _printf_c.c
+++ _printf_c.c
@@ -7,18 +7,19 @@
  * _print_c - A function for printing characters
  * @a: leng
  * @b: char
- * */
+ * Return: Printing charecters
+ **/
 int _print_c(unsigned int *a, char b)
 {
-    if (!b && b != '\0')
-    {
-        write(1, "(null)", 6);
-        *a += 6;
-    }
-    else
-    {
-        write(1, &b, 1);
-        (*a)++;
-    }
-    return (*a);
+	if (!b && b != '\0')
+	{
+		write(1, "(null)", 6);
+		*a += 6;
+	}
+	else
+	{
+		write(1, &b, 1);
+		(*a)++;
+	}
+return (*a);
 }
