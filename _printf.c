243bba4faf4777d85e364050b9d9bc5c21e50fce
sohrab <syusibov92@gmail.com>
2024-03-30 04:07:12 -0700

    we recreated printf(3) function from stdio.h library

---
+++ _printf.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * _printf - this is mini version of ptintf(3) function
+ * @format: a character string
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i, k = 0;
+	va_list ptr;
+
+	va_start(ptr, format);
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
+	{
+		if (format[i] == '%' && format[i + 1] == 'c')
+			k = (unsigned int)_print_c(&k, va_arg(ptr, int)), i++;
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			k = (unsigned int)_print_s(&k, va_arg(ptr, char *));
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+			write(1, "%", 1), i++, k++;
+		else if (format[i] == '%' && format[i + 1] == '\0')
+			continue;
+		else
+			write(1, &format[i], 1), k++;
+	}
+	va_end(ptr);
+	if (k == 0)
+		exit(1);
+	return (k);
+}
